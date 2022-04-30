063e10861214f666fcff3201d5d2e1078082c2f7
Abinet Tamiru <Abinettamiru@outlook.com>
2022-04-30 23:30:23 +0300

    Add project files.

---
+++ Csharp project Q14/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_project_Q14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("insert the first range: ");
+            int first = int.Parse(Console.ReadLine());
+            Console.Write("insert the second range: ");
+            int second = int.Parse(Console.ReadLine());
+
+            for(int x=first; x<=second;x++)
+            {
+                if(x % 5 ==0)
+                {
+                    Console.Write(x + " ");
+                }
+            }
+        }
+    }
+}
